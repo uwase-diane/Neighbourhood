e4fa10823e22c579f9d8554616d679dc7f48e8f2
uwase-diane <diane.uwase13@gmail.com>
2021-02-02 12:51:41 -0500

    added models class

--- hood/models.py
+++ hood/models.py
@@ -1,101 +1,66 @@
 from django.db import models
 from django.contrib.auth.models import User
-import datetime as dt
+from django.utils import timezone
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
-class Profile(models.Model):
-    bio = models.TextField(max_length=100,blank=True)
-    profilepic = models.ImageField(upload_to='profile/',blank= True)
-    email = models.CharField(blank=True, max_length=100)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user
 
 class Neighbourhood(models.Model):
-    Neighbourhood = models.CharField(max_length=30, null=True)
-    Neighbourhood_location = models.CharField(max_length=30, null=True)
-    population = models.PositiveIntegerField(default=0)
-    police_no = models.PositiveIntegerField(default=112)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
+    hood_name = models.CharField(max_length=60, null=True)
+    location = models.CharField(max_length=60)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    hood_image = models.ImageField(upload_to='images/',default='')
+    description = models.TextField(default='')
+    health_tell = models.IntegerField(null=True, blank=True)
+    police_number = models.IntegerField(null=True, blank=True)
 
-    def __str__(self):
-        return self.Neighbourhood
-
-    def create_neighbourhood(self):
+    def create_neighborhood(self):
         self.save()
 
-    def delete_neighbourhood(self):
+    def delete_neighborhood(self):
         self.delete()
 
     @classmethod
-    def get_neighbourhoods(cls):
-        estates = Neighbourhood.objects.all()
-        return estates
-
-    @classmethod
-    def get_specific_hood(cls, id):    
-        chosen_hood = cls.objects.get(id=id)
-        return chosen_hood
-
-    def find_neighbourhood(neighbourhood_id):
-        query = cls.objects.filter(name_icontains = search_term)
-        return query
-
-class Follow(models.Model):
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-    estates = models.ForeignKey(Neighbourhood,on_delete = models.CASCADE)
-
+    def search_by_location(cls, search_term):
+        certain_user=cls.objects.filter(location__icontains=search_term)
+        return certain_user
     def __str__(self):
-        return self.user.username
+        return self.location
+
+class Profile(models.Model):
+    profile_image = models.ImageField(null=True,upload_to='profile_pic/')
+    bio = models.CharField(max_length=300)
+    user = models.OneToOneField(User,on_delete=models.CASCADE)
+    neighbourhoods = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
+
 
     @classmethod
-    def get_following(cls, user_id):
-        following = Follow.objects.filter(user=user_id).all() 
-        return following   
+    def get_profile(cls):
+        all_profiles = cls.objects.all()
+        return all_profiles
 
-
-class Business(models.Model):
-    image = models.ImageField(upload_to = 'business/', null=True, blank=True)
-    project = models.CharField(max_length=30, blank=True)
-    email = models.EmailField(max_length=70, blank=True)
-    estate = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.project
-
-    @classmethod
-    def get_bussinesses(cls):
-        business = cls.objects.all()
-        return business
-
-    @classmethod
-    def get_bussiness_by_estate(cls, neighbourhood_id):
-        messages = cls.objects.all().filter(estate = neighbourhood_id) 
-        return messages
-
-
-class Post(models.Model):
-    image = models.ImageField(upload_to='photos/', null=True)
-    image_name = models.CharField(max_length=30)
-    message = models.TextField(max_length=100, null=True, blank=True)
-    estate = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE, null=True)
-    user_profile = models.ForeignKey(Profile, on_delete = models.CASCADE)
-    user = models.ForeignKey(User, on_delete = models.CASCADE)
-
-
-    def save_post(self):
+    def save_profles(self):
         self.save()
 
-    def delete_post(self):
-        self.delete
+    def delete_profiles(self):
+        self.delete()
 
-    @classmethod
-    def get_posts(cls):
-        messages = cls.objects.all()     
+    def __str__(self):
+        return str(self.user)
 
+class Business(models.Model):
+    business_name = models.CharField(max_length=60, null=True)
+    description = models.CharField(max_length=200,null=True) 
+    user = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+    neighborhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    email = models.EmailField()
+    def create_business(self):
+        self.save()
+    def delete_business(self):
+        self.delete()
 
-    @classmethod
-    def get_posts_by_estate(cls, neighbourhood_id):
-        messages = cls.objects.filter(estate=neighbourhood_id)
-        return messages       
+class Post(models.Model):
+    post = models.CharField(max_length=200,null=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True,blank=True)
+    neighborhood = models.ForeignKey(Neighbourhood,on_delete=models.CASCADE,null=True,blank=True)
